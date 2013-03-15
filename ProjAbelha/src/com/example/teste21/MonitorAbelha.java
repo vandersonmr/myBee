ac1d86e31dfb64074c3567fff4274486ea533a3a
John <john@john-programmer.(none)>
2013-03-15 15:58:12 -0300

    getData e gerarDadoTemperatura implementado

--- ProjAbelha/src/com/example/teste21/MonitorAbelha.java
+++ ProjAbelha/src/com/example/teste21/MonitorAbelha.java
@@ -12,8 +12,8 @@ public class MonitorAbelha {
 		RepaSocket rpa = RepaSocket.getRepaSocket();
 		try {
 			rpa.repaOpen();
-			rpa.registerInterest("apt-chat://message");
-			rpa.repaSend(new RepaMessage("app-chat://message","CAGE GATÃO!".getBytes(),new PrefixAddress()));
+			rpa.registerInterest("cliente");
+			//rpa.repaSend(new RepaMessage("app-chat://message","AE FUNFOU".getBytes(),new PrefixAddress()));
 			rpa.repaClose();
 		} catch (SocketException e) {
 			// TODO Auto-generated catch block
@@ -26,24 +26,26 @@ public class MonitorAbelha {
 	
 	LinkedList<String> listaReq = new LinkedList<String>();
 	// Gera dado randomico
-	private int gerarDadoTempetura(){
-		return (int) (Math.random()*100);
+	private String gerarDadoTemperatura(){
+		return String.valueOf((int)(Math.random()*100))+"º celcius";
 	}
 	
 	
 	// Recebe um string e manda pro servidor.
-	public void sendMsgToServer(){
+	public void sendMsgToServer(String data){
 		
 	}
 	
 	// PARSE da requisição
-	private void getData(){
-		
+	private void getData(String data){
+		if (data.equals("temperatura")){
+			sendMsgToServer(gerarDadoTemperatura());
+		}
 	}
 	
 	// Thread que pega fila e executa
 	private void trataPedidos(){
-		
+		// while LinkedList not null getData
 	}
 	
 	// Thread que ouve servidor e infilera pedido | EU 
