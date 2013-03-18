a55b63faf271ec8257b7992acb1ea89072bb5a46
Vanderson Martins do Rosario <vandersonmr2@gmail.com>
2013-03-18 17:13:10 -0300

    esboço do servidor

--- servidor.c
+++ servidor.c
@@ -4,9 +4,9 @@
 #include <stdbool.h>
 #include <string.h>
 #include <unistd.h>
-
+#include <time.h>
 #include <signal.h>
-
+#include <stdio.h> 
 #include <errno.h>
 #include <pthread.h>
 
@@ -16,6 +16,13 @@
 pthread_t thread;
 
 bool terminated;
+
+FILE *f;
+void saveData(char* addr,char *data){
+	f = fopen(addr,"a+");
+	fprintf(f,"%s\n",data);
+	fclose(f);		
+}
 
 void* handle_message(void* param) {
 	ssize_t read_len = 0;
@@ -30,10 +37,8 @@ void* handle_message(void* param) {
 		read_len = repa_timed_recv(interest, data, prefix_addr, (long int)1E9);
 		if (read_len > 0) {
 			repa_print_prefix(prefix_addr, prefix);
-			printf("Receive %zu from %s I: %s D: %s\n", read_len, prefix, interest, data);
+			saveData(prefix,data);
 		}
-		else
-			printf("nothing\n");
 
 		// Free msg
 		free(interest);
@@ -44,6 +49,20 @@ void* handle_message(void* param) {
 	return 0;
 }
 
+void sendReqToClients(char *req){
+	char *data = req;
+	int data_len = strlen(req);
+	char *interest = (char*)malloc(255*sizeof(char));
+	strcpy(interest,"client");
+        if (data_len > 0) {
+                if (repa_send(interest, data, data_len, 0) < 0) {
+                        printf("CHATERROR: send message (Error No: %d \"%s\").\n", errno, strerror(errno));
+                        exit(EXIT_FAILURE);
+                }
+                printf("Message sent I: \"%s\" D: \"%s\"\n", interest, data);
+        }
+
+}
 
 int main(void) {
 	prefix_addr_t prefix;
@@ -60,32 +79,21 @@ int main(void) {
 	interest = (char*)malloc(255*sizeof(char));
 	data = (char*)malloc(1500*sizeof(char));
 
-	strcpy(interest, "server");    // Interessado em servidor
-	repa_register_interest(interest); // Registra o interesse
-	strcpy(interest, "client"); // Mas envia para os interessados em info-ponto 
-
-	
+	strcpy(interest, "server");
+	repa_register_interest(interest); 
+		
 	pthread_create(&thread, NULL, handle_message, NULL);
 	prefix = repa_node_address();
 	repa_print_prefix(prefix, cprefix);
 	printf("NodePrefix: %d %s\n", prefix, cprefix);
 
-	dummy = fgets(data, 1500, stdin);
-	dummy[strlen(dummy)-1] = '\0';
-	data_len = strlen(data);
-	while (strcmp(data, "exit") != 0) {
-		if (data_len > 0) {
-			if (repa_send(interest, data, data_len, 0) < 0) {
-				printf("CHATERROR: send message (Error No: %d \"%s\").\n", errno, strerror(errno));
-				exit(EXIT_FAILURE);
-			}
-			printf("Message sent I: \"%s\" D: \"%s\"\n", interest, data);
-		}
-		dummy = fgets(data, 1500, stdin);
-		dummy[strlen(dummy)-1] = '\0';
-		data_len = strlen(data);
-	}
+	char *req = malloc(sizeof(char)*11);
+	strcpy(req,"temperatura");
 
+	while(true){
+		sendReqToClients(req);
+		sleep(10);
+	}
 
 	terminated = true;
 	repa_close();
