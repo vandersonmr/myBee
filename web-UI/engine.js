196423ecf6d5bff531614a3ac3c4be862d254dce
Vanderson Martins do Rosario <vandersonmr2@gmail.com>
2013-07-30 20:40:55 -0300

    Adicionado mouse click

--- web-UI/engine.js
+++ web-UI/engine.js
@@ -32,68 +32,106 @@ function timeControl(){
 
 /* Plot Graph code */
 function nodeGraphManager(name){
-	this.name = name;
+	this.name = name
 	this.data = []
-	this.plot = $.plot("#graph", [{data:[],lines:{show:true}}])
-	this.nodeStatus = "OK";	
+	this.plot = $.plot("#graph", [{data:[], label: name+" temp."}],
+			              {series: {
+					lines:{show:true}, 
+					points:{show:true}},
+				       grid: {
+					hoverable: true,
+					clickable: true}
+				      })
+	this.nodeStatus = "OK"
 
 	this.setData = function(data){
-		this.data = data; 		
+		this.data = data 		
 	}
 	
 	this.setNodeStatus = function(nodeStatus){
-		this.nodeStatus = nodeStatus;
-		$("#status").html(nodeStatus);
+		this.nodeStatus = nodeStatus
+		$("#status").html(nodeStatus)
 	}
 		
 	this.update = function(){
-		this.plot.setData([ this.data ]);
-		this.plot.setupGrid();
-		this.plot.draw();
+		this.plot.setData([ this.data ])
+		this.plot.setupGrid()
+		this.plot.draw()
 	}
+
+	var previousPoint = null;
+	$("#graph").bind("plothover", function (event, pos, item) {
+		if (item) {
+			if (previousPoint != item.dataIndex) {
+				previousPoint = item.dataIndex;
+				$("#tooltip").remove();
+				var x = item.datapoint[0].toFixed(2),
+				y = item.datapoint[1].toFixed(2);
+				showTooltip(item.pageX, item.pageY,
+				     "As "+ x + " temperatura de  " + y+ " graus célcius");
+			}
+		} else {
+			$("#tooltip").remove();
+			previousPoint = null;            
+		}		
+
+	});
  
 }
 
 var temperatures = [], totalPoints = 100;
 function parseData(input) {
 	if(temperatures.length > 0)
-		temperatures = temperatures.splice(1);
+		temperatures = temperatures.splice(1)
 
 	var rows = input.split("<br>");
 	for(var captura=1; captura < rows.length; ++captura){
-		var temp = rows[captura].split("&")[2];
+		var temp = rows[captura].split("&")[2]
 
-		temperatures.push(temp);
+		temperatures.push(temp)
 
 	}
 
 	var res = [];
 	for (var i = 0; i < temperatures.length; ++i)
 		res.push([i, temperatures.pop()])
-			return res;
+			return res
+}
+
+function showTooltip(x, y, contents) {
+	$("<div id='tooltip'>" + contents + "</div>").css({
+		position: "absolute",
+		display: "none",
+		top: y + 5,
+		left: x + 5,
+		border: "1px solid #fdd",
+		padding: "2px",
+		"background-color": "#fee",
+		opacity: 0.80
+	}).appendTo("body").fadeIn(200);
 }
 
 var updateInterval = 2000;
 
 function update() {
-	var input;
+	var input
 	$.get('cgi-bin/getDados').success(
 			function(data){	
-			var node = window.node
-			node.setNodeStatus(data.split("<br>")[0])
-			node.setData(parseData(data));
-			node.update();
+				var node = window.node
+				node.setNodeStatus(data.split("<br>")[0])
+				node.setData(parseData(data))
+				node.update()
 			});
 
-	setTimeout(update, updateInterval);
+	setTimeout(update, updateInterval)
 }
 /* ---------------------------------------*/
 
 /* Main */
 jQuery(document).ready(new function () {
-		var tc = new timeControl();
-		tc.startClockUpdate();
-		var node1 = new nodeGraphManager();
-		window.node = node1;
-		update();
-		});
+		var tc = new timeControl()
+		tc.startClockUpdate()
+		var node1 = new nodeGraphManager()
+		window.node = node1
+		update()
+});
