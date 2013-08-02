eb9647305ff049288108a37596b4a31ed59c0342
Vanderson Martins do Rosario <vandersonmr2@gmail.com>
2013-08-02 10:04:25 -0300

    Fix sql

--- src/database/dbname.sql
+++ src/database/dbname.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS `temperatures`;
 CREATE TABLE `temperatures` (
   `Prefix` varchar(255) DEFAULT NULL,
   `Date` varchar(255) DEFAULT NULL,
-  `Temperature` varchar(255) DEFAULT NULL
+  `Temperature` varchar(255) DEFAULT NULL,
   `Status` int(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
