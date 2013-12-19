aee8437aaa8682ad7877814aaf38bfde39b15b6c
Vanderson M. do Rosario <vandersonmr2@gmail.com>
2013-12-19 17:02:51 -0200

    Sql

--- src/database/dbname.sql
+++ src/database/dbname.sql
@@ -1,10 +1,19 @@
 
-DROP TABLE IF EXISTS `temperatures`;
+DROP TABLE IF EXISTS `nodesOnline`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `nodesOnline` (
+  `nodeID` varchar(200) DEFAULT NULL,
+  UNIQUE KEY `tb_un` (`nodeID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+DROP TABLE IF EXISTS `temperatures`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `temperatures` (
   `Prefix` varchar(255) DEFAULT NULL,
   `Date` varchar(255) DEFAULT NULL,
   `Temperature` varchar(255) DEFAULT NULL,
-  `Status` int(10) DEFAULT NULL
+  `Status` int(10) DEFAULT NULL,
+  `nodeIP` varchar(200) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
