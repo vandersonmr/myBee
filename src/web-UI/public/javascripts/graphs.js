18c1d711d965b29a54e3c2fc2c35bb0cc07df5cc
vandersonmr <vandersonmr2@gmail.com>
2014-01-18 15:45:57 -0200

    Estatísticas

--- src/web-UI/public/javascripts/graphs.js
+++ src/web-UI/public/javascripts/graphs.js
@@ -57,6 +57,7 @@ function nodeGraphManager(name, divId, options){
                         "<ul>"+
                           "<li><a href=\"#tabs-1"+name+"\">Gráfico</a></li>"+
                           "<li><a href=\"#tabs-2"+name+"\">Dados plano</a></li>"+
+                          "<li><a href=\"#tabs-3"+name+"\">Estatísticas</a></li>"+
                         "</ul>" : "") +
             
           (options.tabs ? "<div id=\"tabs-1"+name+"\" style=\"width:90%;height:75%\">" : "")+
@@ -69,6 +70,9 @@ function nodeGraphManager(name, divId, options){
 
           (options.tabs ? "<div id=\"tabs-2"+name+"\" style=\"width:90%;height:77%\">" : "")+
           (options.tabs ? "<textarea id=\"rawData"+name+"\" readonly style=\"resize: none; width:100%;height:100%\"></textarea>" : "")+
+          (options.tabs ? "</div>" : "")+
+
+          (options.tabs ? "<div id=\"tabs-3"+name+"\" style=\"width:90%;height:77%\">" : "")+
           (options.tabs ? "</div>" : "")+
           "</div>");
 
@@ -95,16 +99,70 @@ function nodeGraphManager(name, divId, options){
     }
   }
   
+  this.interateAsyncData = function(callBack, endFunction, result) {
+    function loop(key,data,times,stats) {
+      if (key == 0) {
+        if (endFunction)
+          endFunction()
+        else
+          return
+      }
+      setTimeout(function() {
+        callBack(key,data,times,stats)
+        loop(--key,data,times,stats)
+      },1);
+    }
+
+    loop(this.data.length-1,this.data,times,stats);
+  }
+
+  this.fillDataStatistics = function(){
+    $("#"+divId+" #tabs-3"+name).append("<h3> Estatísticas dos dados </h3>"+
+                                        " <br><h2> Carregando... </h2>");
+  
+    var result = {mean:0,alerts:0}
+    this.interateAsyncData(
+        function(key, data, times, stats) {
+            var dataInt = parseInt(data[key][1])
+            result.mean += dataInt/data.length;
+            
+            if (!result.highest) {
+              result.highest = dataInt
+            } else {
+              if (dataInt > result.highest)
+                result.highest = dataInt
+            }
+
+            if (!result.lowest) {
+              result.lowest = dataInt
+            } else {
+              if (dataInt < result.lowest)
+                result.lowest = dataInt
+            }
+
+            if (stats[key] != 0)
+              result.alerts += 1
+        },
+        function() {
+          $("#"+divId+" #tabs-3"+name).empty()
+          $("#"+divId+" #tabs-3"+name).append("<h3> Estatísticas dos dados </h3>")
+          $("#"+divId+" #tabs-3"+name).append("<li> Temperatura Média : "+Math.round(result.mean)+" </li>")
+          $("#"+divId+" #tabs-3"+name).append("<li> Temperatura Máxima : "+result.highest+" </li>")
+          $("#"+divId+" #tabs-3"+name).append("<li> Temperatura Mínima : "+result.lowest+" </li>")
+          $("#"+divId+" #tabs-3"+name).append("<li> Quantidade de alertas : "+result.alerts+" </li>")
+        },
+        result
+    )
+
+  }
+
   this.fillDataTextArea = function(){
     $("#"+divId+" #rawData"+name).empty()
     $("#"+divId+" #rawData"+name).append("Chave  Tempo dado  status\n");
-    (function loop(key,data,times,stats) {
-      if (key == 0) return
-      setTimeout(function() {
-        $("#"+divId+" #rawData"+name).append(key+" "+times[key]+" "+data[key][1]+" "+stats[key]+"\n")
-        loop(--key,data,times,stats)
-      },10);
-    })(this.data.length-1,this.data,times,stats);
+
+    this.interateAsyncData(function(key, data, times, stats) {
+      $("#"+divId+" #rawData"+name).append(key+" "+times[key]+" "+data[key][1]+" "+stats[key]+"\n")
+    })
   }
 
   this.setData = function(data, time, stat){
@@ -126,7 +184,10 @@ function nodeGraphManager(name, divId, options){
     this.highlight()
     $("#"+divId+" [id='g"+name+"']").show()
     $("#"+divId+" #lastTemp"+name).html(this.data[this.data.length-1][1])
-    if(options.tabs) this.fillDataTextArea()
+    if(options.tabs) {
+      this.fillDataTextArea()
+      this.fillDataStatistics()
+    }
   }
 
   if (options.closeBox)
