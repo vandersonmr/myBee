c54c923529bafcc0e312bfa4387066e410c1cb9c
vandersonmr <vandersonmr2@gmail.com>
2014-02-16 17:17:41 -0300

    MonitorAPI implementado. Tudo parou de funcionar?

--- src/server.cpp
+++ src/server.cpp
@@ -1,58 +1,15 @@
-#include <mysql.h>
-#include <iostream>
+#include "monitorAPI/include/server_monitor.hpp"
 
-#include "machineLearning.h"
-#include "dataDAO.hpp"
-#include "hdr/repa.h"
-#include "repaAPI/include/repa_api.hpp"
-
-RepaAPI<Data> repa;
-
-int checkData(Data data){
-  return testData(data);
-}
-
-void updateListOfNodesOnline() {
-  vector<string> nodes = repa.GetNodesOnline();
-  clearNodesOnline();
-  for (string nodeOnline : nodes) {
-    insertNodeOnline(nodeOnline); 
-  }
-}
-
-void handle_message(message<Data> msg) {       
-
-  for (Data data: msg.data){
-    int status;
-    status = checkData(data);
-    cout << "message: " << data.value;
-
-    data.node = msg.prefix_address;
-    saveData(data,status);
-
-    cout <<" \" Status: \"" << status << "\" Prefix: " << 
-      data.nickname << "\n\n";
-
-    updateListOfNodesOnline();
-  }
-}
+Data filter(Data d) {
+  cout << "\" Prefix: " <<
+        d.nickname << " send a msg\n\n";
+  return d;
+} 
 
 int main(void) {
-
-  if (!connectDatabase()){
-    return EXIT_FAILURE;
-  }
-
-  vector<string> interests;
-  interests.push_back(string("server"));
-  
-  if (!repa.InitRepa(interests)) return EXIT_FAILURE;
-
-  repa.GetMessage(&handle_message);
-
-  while(true) sleep(1);
-
-  closeConnection();
-  repa.CloseRepa();
-  exit(EXIT_SUCCESS);
+  ServerMonitor server;
+  server.EnablePersistence("config/db.conf");
+  server.EnableMachineLearning(1);
+  server.SetFilter(&filter);
+  while(true) { sleep(1); };
 }
