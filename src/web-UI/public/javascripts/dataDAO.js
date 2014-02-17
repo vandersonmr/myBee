0bc591ef23b624b14bd059b816cbb1a4aa7aecbb
vandersonmr <vandersonmr2@gmail.com>
2014-02-17 20:23:20 -0300

    plotando pelo menos

--- src/web-UI/public/javascripts/dataDAO.js
+++ src/web-UI/public/javascripts/dataDAO.js
@@ -4,6 +4,7 @@ exports.dataDAO = function(mysql) {
     var queryText = "select * from (select *,str_to_date(Date,'%a %b %e %H:%i:%s %Y') "
       + "as Time from data) as t INNER JOIN nodesOnline ON t.nodeIP="
       + "nodesOnline.nodeID where t.Time > NOW() - INTERVAL "+minutes+" MINUTE ORDER BY Time DESC;";
+    
     mysql.query(queryText,
         function(err, result, fields) {
           if (err) throw err;
