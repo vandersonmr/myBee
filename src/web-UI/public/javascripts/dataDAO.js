8cd0db55d0840d9dd0a0158e29d1ca8a93d3bc15
vandersonmr <vandersonmr2@gmail.com>
2014-02-24 22:19:15 -0300

    Bug no nodesOnline tapiado

--- src/web-UI/public/javascripts/dataDAO.js
+++ src/web-UI/public/javascripts/dataDAO.js
@@ -1,10 +1,14 @@
 exports.dataDAO = function(mysql) {
 
   this.getLastsDatasByMinute = function(minutes, callBack) {  
-    var queryText = "select * from (select *,str_to_date(Date,'%a %b %e %H:%i:%s %Y') "
+    //TODO! <--BUG HERE
+    /*var queryText = "select * from (select *,str_to_date(Date,'%a %b %e %H:%i:%s %Y') "
       + "as Time from data) as t INNER JOIN nodesOnline ON t.nodeIP="
-      + "nodesOnline.nodeID where t.Time > NOW() - INTERVAL "+minutes+" MINUTE ORDER BY Time DESC;";
-    
+      + "nodesOnline.nodeID where t.Time > NOW() - INTERVAL "+minutes+" MINUTE ORDER BY Time DESC;";*/
+
+    var queryText = "select * from (select *,str_to_date(Date,'%a %b %e %H:%i:%s %Y') "
+      + "as Time from data) as t where t.Time > NOW() - INTERVAL "+minutes+" MINUTE ORDER BY Time DESC;";
+
     mysql.query(queryText,
         function(err, result, fields) {
           if (err) throw err;
