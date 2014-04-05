f9ea9f436d84b760bc1115dd65c02f4f6ab0b9e7
vandersonmr <vandersonmr2@gmail.com>
2014-04-05 19:11:52 -0300

    smart pointers

--- src/machineLearning/machineLearning.cpp
+++ src/machineLearning/machineLearning.cpp
@@ -4,23 +4,22 @@
 #include "include/KCentroid.h"
 #include "include/Node.h"
 #include <unordered_map>
+#include <memory>
 
 using namespace std;
 
 
-unordered_map<string,Node*> Nodes;
+unordered_map<string,unique_ptr<Node>> Nodes;
 
 int testData(Data data){
   int resLimit = testLimit(data);
   string nodeName = string(data.nickname+data.type);
 
-  if(Nodes[nodeName] == NULL)
-    Nodes[nodeName] = new Node();
+  if(Nodes[nodeName] == nullptr)
+    Nodes[nodeName] = unique_ptr<Node>(new Node());
 
-  Node* node = Nodes[nodeName];
-
-  int resKCentroid = node->kcentroidControl.testData(data,data.nickname);
-  int resKrls      = node->krlsControl.testData (data,data.nickname);
+  int resKCentroid = Nodes[nodeName]->kcentroidControl.testData(data,data.nickname);
+  int resKrls      = Nodes[nodeName]->krlsControl.testData (data,data.nickname);
 
   return resLimit | resKCentroid | resKrls;
 }
