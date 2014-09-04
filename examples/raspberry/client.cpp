53122fbe626574ee9b57904ad1a56afd45084c25
João Almeida Jr <joao29a@gmail.com>
2014-09-04 00:05:01 -0300

    Mudança do cliente no raspberry

--- examples/raspberry/client.cpp
+++ examples/raspberry/client.cpp
@@ -8,7 +8,6 @@
 #include <string>
 #include <bcm2835.h>
 
-ClientMonitor *tempMonitor;
 DHT22 dht(1);
 
 double GetTemperature(){
@@ -19,33 +18,15 @@ double GetHumidity(){
     return dht.get_humidity();
 }
 
-void handler(int sig){
-    cout << "Signal " << sig << endl;
-    tempMonitor->Close();
-}
-
 int main(int argc, char **argv) {
-  char* nickname = argv[argc-1];
-
-  if (nickname == 0 || argc == 1) {
-    printf("Formato incorreto! Tente ./client nomeDoNode\n");
-    exit(1);
-  }
-
   dht.init();
 
-  ClientMonitor monitor(string(nickname), 60);
-
-  tempMonitor = &monitor;
+  ClientMonitor monitor(&argc, argv);
 
   monitor.AddDataGenerator("temperature", &GetTemperature);
   monitor.AddDataGenerator("humidity"   , &GetHumidity);
 
-  signal(SIGINT, &handler);
-  
-  while(true){
-    sleep(1);
-  }
+  monitor.Run();
 
   return 0;
 }
