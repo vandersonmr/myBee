1156995af2c7bf56e5ee9dbd3d4d37122c27baea
João Almeida Jr <joao29a@gmail.com>
2015-07-24 00:10:39 -0300

    pop-up pra selecionar formato

--- src/web-UI/routes/exportData.js
+++ src/web-UI/routes/exportData.js
@@ -3,13 +3,17 @@
  */
 
 exports.exportData = function(req, res) {
+  var query = req.params.mode.split('=');
   function renderize(data){
-    res.render('exportData', { layout : false, result : data })
+    if (query[1] === 'csv')
+      res.render('exportData', { layout : false, result : data })
+    else if (query[1] === 'pdf')
+      res.render('exportDataPDF', { layout : false, result : data })
   }
-  if (req.params.mode === 'all') {
+  if (query[0] === 'all') {
     dataDAO.getAllValues(renderize);
   } else {
-    var request = req.params.mode.split('&');
+    var request = query[0].split('&');
     if (request.length == 0) return;
     var node_name = request.shift();
     dataDAO.getValuesFrom(node_name, request, renderize);
