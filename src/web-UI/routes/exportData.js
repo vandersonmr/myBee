cf8f196d182a7aa58fb80f8c692aee7a2381229a
João Almeida Jr <joao29a@gmail.com>
2015-07-30 21:57:56 -0300

    calculo variancia e dp

--- src/web-UI/routes/exportData.js
+++ src/web-UI/routes/exportData.js
@@ -5,8 +5,50 @@
 exports.exportData = function(req, res) {
   var query = req.params.mode.split('=');
   function renderize(data){
+    
+    /* calculo da temperatura e humidade media */
+    var temperatura_total = 0.0;
+    var humidade_total = 0.0;
+    var qtde_temp = 0;
+    var qtde_hum = 0;
+    for (var i = 0; i < data.length; i++) {
+      if (data[i].Type === 'temperature') {
+        temperatura_total += parseFloat(data[i].Value);
+        qtde_temp += 1;
+      } else if (data[i].Type === 'humidity') {
+        humidade_total += parseFloat(data[i].Value);
+        qtde_hum += 1;
+      }
+    }
+
+    /* calculo da variancia da temperatura e humidade */
+    var temperatura_variancia = 0.0;
+    var humidade_variancia = 0.0;
+    for (var i = 0; i < data.length; i++) {
+      if (data[i].Type === 'temperature') {
+        temperatura_variancia += Math.pow(parseFloat(data[i].Value)
+            - (temperatura_total / qtde_temp), 2);
+      } else if (data[i].Type === 'humidity') {
+        humidade_variancia += Math.pow(parseFloat(data[i].Value)
+          - (humidade_total / qtde_hum), 2);
+
+      }
+    }
+    temperatura_variancia /= qtde_temp;
+    humidade_variancia /= qtde_temp;
+
+    var data_json = {
+      data: data,
+      temperatura_media: temperatura_total / qtde_temp,
+      temperatura_variancia: temperatura_variancia,
+      temperatura_desvio: Math.sqrt(temperatura_variancia),
+      humidade_media: humidade_total / qtde_hum,
+      humidade_variancia: humidade_variancia,
+      humidade_desvio: Math.sqrt(humidade_variancia)
+    };
+
     if (query[1] === 'csv')
-      res.render('exportData', { layout : false, result : data })
+      res.render('exportData', { layout : false, result : data_json })
     else if (query[1] === 'pdf')
       res.render('exportDataPDF', { layout : false, result : data })
   }
